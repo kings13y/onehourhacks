49f8f8b3c74943d0d24c3cb35323ca012f12e863
Kingsley Davies <kingsley@d-limitd.com>
2017-02-27 09:54:07 +0000

    Added a template directory for default sbt file and test template.

---
+++ template/build.sbt
@@ -0,0 +1,10 @@
+organization := "org.onehourhacks"
+name := "template"
+version := "0.1-SNAPSHOT"
+scalaVersion := "2.12.1"
+
+libraryDependencies ++= Seq(
+  "org.scalatest" %% "scalatest" % "3.0.1" % "test"
+)
+
+libraryDependencies += "org.slf4j" % "slf4j-simple" % "1.6.4"
---
+++ template/src/test/scala/FizzBuzzSpec.scala
@@ -0,0 +1,16 @@
+package hacks.fizzbuzz
+
+import org.scalatest.{FunSpec, Matchers}
+
+class FizzBuzzSpec extends FunSpec with Matchers {
+  
+  describe("A FizzBuzz processor") {
+    
+    it("should return 'Fizz' from a multiple of three only") { 
+        ???
+        // TheProcessor.process(3) should equal ("Fizz") 
+    }
+    
+  }
+
+}
